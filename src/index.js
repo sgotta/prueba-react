a7356090eef41d7a2417b41c695629ead4662472
Simón Gotta <simon.gotta@gmail.com>
2020-09-04 12:21:53 -0300

    Functional components

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Card from "./components/Card";
+import Welcome from "./components/Welcome";
 const container = document.getElementById("root");
 
 // ReactDOM.render(__QUE__, __DONDE__)
 ReactDOM.render(
   <React.StrictMode>
-    <Card
-      title="Technique Guides"
-      description="Learn amazing street workout and calisthenics"
-      img="https://raw.githubusercontent.com/rulpalacios/FitnessApp/styles/src/images/exercise.png"
-      leftColor="#a74cf2"
-      rightColor="#617bfb"
-    />
+    <div>
+      <Welcome username="Simón" />
+      <Card
+        title="Technique Guides"
+        description="Learn amazing street workout and calisthenics"
+        img="https://raw.githubusercontent.com/rulpalacios/FitnessApp/styles/src/images/exercise.png"
+        leftColor="#a74cf2"
+        rightColor="#617bfb"
+      />
+    </div>
   </React.StrictMode>,
   container
 );
