ee1fe7f3bce7d0d59781f5f71e3a6eda99a04db4
Simón Gotta <simon.gotta@gmail.com>
2020-09-26 13:07:40 -0300

    ExerciseList using destructuring

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -4,17 +4,7 @@ import Card from "./Card";
 const ExerciseList = ({ exercises }) => (
   <React.Fragment>
     {exercises.map((exercise) => {
-      return (
-        <Card
-          key={exercise.id}
-          title={exercise.title}
-          description={exercise.description}
-          //img="https://raw.githubusercontent.com/rulpalacios/FitnessApp/styles/src/images/exercise.png"
-          img={exercise.img}
-          leftColor={exercise.leftColor}
-          rightColor={exercise.rightColor}
-        />
-      );
+      return <Card key={exercise.id} {...exercise} />;
     })}
   </React.Fragment>
 );
